66ea4c4e5873c4a14b406c5b0d32ed7099928f28
Nils Nordström <nils.nordstrom@elev.ga.ntig.se>
2024-03-04 17:59:23 +0100

    Klar med sidan, ska kanske göra JS om jag känner för det

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hgdhad");
+let kundvagn = [];
+
+function ha(artikel){
+    console.log(artikel);
+    kundvagn.push(artikel);
+}
+
+function visaKundvagn(){
+    for(let i = 0; i < kundvagn.length; ++i){
+        console.log("item " + (i+1) + ": " + kundvagn[i]);
+    }
+}
