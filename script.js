bcce6d12d63852741f96d915520c2a8a197ab855
Nils Nordström <nils.nordstrom@elev.ga.ntig.se>
2024-03-06 12:49:27 +0100

    Påbörjat kundvagn och JS

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 let kundvagn = [];
+let kundvagnDiv = document.getElementById("kundvagnDiv");
 
 function ha(artikel){
     console.log(artikel);
     kundvagn.push(artikel);
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(artikel));
+    ul.appendChild(li);
+  
 }
 
 function visaKundvagn(){
     for(let i = 0; i < kundvagn.length; ++i){
         console.log("item " + (i+1) + ": " + kundvagn[i]);
     }
+    
+    if (kundvagnDiv.style.display === "none") {
+        kundvagnDiv.style.display = "block";
+      } else {
+        kundvagnDiv.style.display = "none";
+      }
 }
